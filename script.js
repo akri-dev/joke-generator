01025e35dd10d882568d470566981848bd9f0241
Alec Joseph Burio <alecjoseph.dev@gmail.com>
2023-05-24 13:00:06 +0800

    finished project

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ jokes[2] = "Chuck Norris has a mug of nails instead of coffee in the morning."
 jokes[3] = "Chuck Norris does not sleep."
 jokes[4] = "Chuck waits."
 
+//functions will now consider the data of this variable rather than the first one
+// defined the array to be streamlined
+var jokes = [
+    "It’s gonna be legen – wait for it, and I hope you’re not lactose intolerant because the second half of that word is… dairy!",
+    "Can I have your phone number? It’s for the bride.",
+    "The question is, do these strategies ever not work for me? Either way, the answer's about half the time.",
+    "You don't bring a date to a wedding. That's like taking a deer carcass on a hunting trip.",
+    "There are so many things to do with the human mouth. Why waste it on talking?"
+] // array numbers are automatically assigned
+
 //define function
 var randomNumZtoOne = Math.random()
 console.log(randomNumZtoOne)
